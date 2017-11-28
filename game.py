30c85af1b062a726fcb986100bdcde2cc205d30f
BOI <You know me>
2017-11-28 10:24:40 -0800

    f
    f

--- game.py
+++ game.py
@@ -1,17 +1,9 @@
 ###Matt Gonzalez###
-<<<<<<< HEAD
-####Grant Tong###
-=======
 ###Grant Tong###
->>>>>>> b9e240f07c369ebc8d43f670fb06b63c4be0742c
 ###Period 4###
 
 HERO_HEALTH = 100
 MONSTER_HEALTH = 50
 
 def main():
-<<<<<<< HEAD
-    print:
-=======
     print("
->>>>>>> b9e240f07c369ebc8d43f670fb06b63c4be0742c
